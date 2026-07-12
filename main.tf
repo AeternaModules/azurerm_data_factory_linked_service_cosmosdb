681c656ebd10c0d8c523114b621e4360d2065460
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:58:10 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,13 +1,23 @@
+data "azurerm_key_vault_secret" "account_key" {
+  for_each     = { for k, v in var.data_factory_linked_service_cosmosdbs : k => v if v.account_key_key_vault_id != null && v.account_key_key_vault_secret_name != null }
+  name         = each.value.account_key_key_vault_secret_name
+  key_vault_id = each.value.account_key_key_vault_id
+}
+data "azurerm_key_vault_secret" "connection_string" {
+  for_each     = { for k, v in var.data_factory_linked_service_cosmosdbs : k => v if v.connection_string_key_vault_id != null && v.connection_string_key_vault_secret_name != null }
+  name         = each.value.connection_string_key_vault_secret_name
+  key_vault_id = each.value.connection_string_key_vault_id
+}
 resource "azurerm_data_factory_linked_service_cosmosdb" "data_factory_linked_service_cosmosdbs" {
   for_each = var.data_factory_linked_service_cosmosdbs
 
   data_factory_id          = each.value.data_factory_id
   name                     = each.value.name
   account_endpoint         = each.value.account_endpoint
-  account_key              = each.value.account_key
+  account_key              = each.value.account_key != null ? each.value.account_key : try(data.azurerm_key_vault_secret.account_key[each.key].value, null)
   additional_properties    = each.value.additional_properties
   annotations              = each.value.annotations
-  connection_string        = each.value.connection_string
+  connection_string        = each.value.connection_string != null ? each.value.connection_string : try(data.azurerm_key_vault_secret.connection_string[each.key].value, null)
   database                 = each.value.database
   description              = each.value.description
   integration_runtime_name = each.value.integration_runtime_name
--- variables.tf
+++ variables.tf
@@ -7,9 +7,13 @@ Required:
 Optional:
     - account_endpoint
     - account_key
+    - account_key_key_vault_id (alternative to account_key - read from Key Vault instead)
+    - account_key_key_vault_secret_name (alternative to account_key - read from Key Vault instead)
     - additional_properties
     - annotations
     - connection_string
+    - connection_string_key_vault_id (alternative to connection_string - read from Key Vault instead)
+    - connection_string_key_vault_secret_name (alternative to connection_string - read from Key Vault instead)
     - database
     - description
     - integration_runtime_name
@@ -17,17 +21,21 @@ Optional:
 EOT
 
   type = map(object({
-    data_factory_id          = string
-    name                     = string
-    account_endpoint         = optional(string)
-    account_key              = optional(string)
-    additional_properties    = optional(map(string))
-    annotations              = optional(list(string))
-    connection_string        = optional(string)
-    database                 = optional(string)
-    description              = optional(string)
-    integration_runtime_name = optional(string)
-    parameters               = optional(map(string))
+    data_factory_id                         = string
+    name                                    = string
+    account_endpoint                        = optional(string)
+    account_key                             = optional(string)
+    account_key_key_vault_id                = optional(string)
+    account_key_key_vault_secret_name       = optional(string)
+    additional_properties                   = optional(map(string))
+    annotations                             = optional(list(string))
+    connection_string                       = optional(string)
+    connection_string_key_vault_id          = optional(string)
+    connection_string_key_vault_secret_name = optional(string)
+    database                                = optional(string)
+    description                             = optional(string)
+    integration_runtime_name                = optional(string)
+    parameters                              = optional(map(string))
   }))
   validation {
     condition = alltrue([
