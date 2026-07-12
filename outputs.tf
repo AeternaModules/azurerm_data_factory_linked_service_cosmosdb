463f9879b5489721c1bfeb1cf88c61da9c6fed91
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,52 +1,47 @@
-output "data_factory_linked_service_cosmosdbs" {
-  description = "All data_factory_linked_service_cosmosdb resources"
-  value       = azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs
-  sensitive   = true
-}
 output "data_factory_linked_service_cosmosdbs_account_endpoint" {
-  description = "List of account_endpoint values across all data_factory_linked_service_cosmosdbs"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : v.account_endpoint]
+  description = "Map of account_endpoint values across all data_factory_linked_service_cosmosdbs, keyed the same as var.data_factory_linked_service_cosmosdbs"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : k => v.account_endpoint }
 }
 output "data_factory_linked_service_cosmosdbs_account_key" {
-  description = "List of account_key values across all data_factory_linked_service_cosmosdbs"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : v.account_key]
+  description = "Map of account_key values across all data_factory_linked_service_cosmosdbs, keyed the same as var.data_factory_linked_service_cosmosdbs"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : k => v.account_key }
   sensitive   = true
 }
 output "data_factory_linked_service_cosmosdbs_additional_properties" {
-  description = "List of additional_properties values across all data_factory_linked_service_cosmosdbs"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_linked_service_cosmosdbs, keyed the same as var.data_factory_linked_service_cosmosdbs"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : k => v.additional_properties }
 }
 output "data_factory_linked_service_cosmosdbs_annotations" {
-  description = "List of annotations values across all data_factory_linked_service_cosmosdbs"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : v.annotations]
+  description = "Map of annotations values across all data_factory_linked_service_cosmosdbs, keyed the same as var.data_factory_linked_service_cosmosdbs"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : k => v.annotations }
 }
 output "data_factory_linked_service_cosmosdbs_connection_string" {
-  description = "List of connection_string values across all data_factory_linked_service_cosmosdbs"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : v.connection_string]
+  description = "Map of connection_string values across all data_factory_linked_service_cosmosdbs, keyed the same as var.data_factory_linked_service_cosmosdbs"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : k => v.connection_string }
   sensitive   = true
 }
 output "data_factory_linked_service_cosmosdbs_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_linked_service_cosmosdbs"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_linked_service_cosmosdbs, keyed the same as var.data_factory_linked_service_cosmosdbs"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : k => v.data_factory_id }
 }
 output "data_factory_linked_service_cosmosdbs_database" {
-  description = "List of database values across all data_factory_linked_service_cosmosdbs"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : v.database]
+  description = "Map of database values across all data_factory_linked_service_cosmosdbs, keyed the same as var.data_factory_linked_service_cosmosdbs"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : k => v.database }
 }
 output "data_factory_linked_service_cosmosdbs_description" {
-  description = "List of description values across all data_factory_linked_service_cosmosdbs"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : v.description]
+  description = "Map of description values across all data_factory_linked_service_cosmosdbs, keyed the same as var.data_factory_linked_service_cosmosdbs"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : k => v.description }
 }
 output "data_factory_linked_service_cosmosdbs_integration_runtime_name" {
-  description = "List of integration_runtime_name values across all data_factory_linked_service_cosmosdbs"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : v.integration_runtime_name]
+  description = "Map of integration_runtime_name values across all data_factory_linked_service_cosmosdbs, keyed the same as var.data_factory_linked_service_cosmosdbs"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : k => v.integration_runtime_name }
 }
 output "data_factory_linked_service_cosmosdbs_name" {
-  description = "List of name values across all data_factory_linked_service_cosmosdbs"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : v.name]
+  description = "Map of name values across all data_factory_linked_service_cosmosdbs, keyed the same as var.data_factory_linked_service_cosmosdbs"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : k => v.name }
 }
 output "data_factory_linked_service_cosmosdbs_parameters" {
-  description = "List of parameters values across all data_factory_linked_service_cosmosdbs"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : v.parameters]
+  description = "Map of parameters values across all data_factory_linked_service_cosmosdbs, keyed the same as var.data_factory_linked_service_cosmosdbs"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb.data_factory_linked_service_cosmosdbs : k => v.parameters }
 }
 
